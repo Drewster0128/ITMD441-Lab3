61e7aa2f0d0e80ad0ff7b143c5d7fae4e7ad79fd
Drewster0128 <acook21@hawk.iit.edu>
2025-02-27 15:28:02 -0600

    Complete code for exercise 1

---
+++ acook-lab3.js
@@ -0,0 +1,31 @@
+// Andrew Cook
+// ITMD 441
+
+//Exercise 1
+function minMaxAverage(lst) {
+    let max =  null;
+    let min = null;
+    let count = 0;
+    let sum = 0;
+
+    for(let number of lst)
+    {
+        if(max === null || number > max)
+        {
+            max = number;
+        }
+        else if(min === null || number < min)
+        {
+            min = number;
+        }
+
+        count++;
+        sum += number;
+    }
+
+    let avg = sum/count;
+
+    console.log(`Total Numbers: ${count}, Min Value: ${min}, Max Value: ${max}, Average: ${avg}`)
+}
+
+minMaxAverage([2, 5, 23, 6, 9, 4, 30, 1])
